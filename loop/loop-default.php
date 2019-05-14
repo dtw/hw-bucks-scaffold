6805c1d4f2e3990128309e982152c0da5fe05584
dtw <dtw@users.noreply.github.com>
2019-05-14 20:38:09 +0100

    Whitespace

--- loop/loop-default.php
+++ loop/loop-default.php
@@ -5,17 +5,17 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<?php get_template_part('elements/custom-fields-block'); ?>
-        
+
 	<?php the_content(); ?>
 
 		<?php the_tags( '<div class="the-tags"><p>Explore more ', ' ', '</p></div>' ); ?>
 
 	<?php endwhile; ?>
-	
-			<?php get_template_part('elements/pagination'); ?>    
-    
+
+			<?php get_template_part('elements/pagination'); ?>
+
 	<?php else: ?>
 		<p>Sorry, nothing found.</p>
 	<?php endif; ?>
-	
+
    </div><!-- end of Loop Default -->
