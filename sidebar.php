bbc2fdd06e62754abf71eaeba2aad211e344a054
dtw <dtw@users.github.com>
2019-05-22 13:34:58 +0100

    Whitespace
    I mean...

--- sidebar.php
+++ sidebar.php
@@ -1,69 +1,44 @@
 <?php if ( is_tax('') || is_singular('local_services') || is_search() || is_page_template('template-find-services.php') ) { ?>
 	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Services Sidebar')) : endif; ?>
-		<?php } ?>
-
+<?php } ?>
 
 <?php if ( is_home()  ) { ?>
 	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Home Right')) : endif; ?>
-		<?php } ?>
-
+<?php } ?>
 
 <?php if (is_page()) { ?>
-
 	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Pages')) : endif; ?>
-   		<?php } ?>
-
-
+<?php } ?>
 
 <!-- show sub-category navigation, if necessary -->
 <?php if ( is_archive() ) { ?>
- 
-     
-<?php $this_cat = (get_query_var('cat')) ? get_query_var('cat') : 1; ?>
-
-<?php $this_category = get_category($this_cat);
+	<?php $this_cat = (get_query_var('cat')) ? get_query_var('cat') : 1; ?>
+	<?php $this_category = get_category($this_cat);
 	if ( $this_category->parent ) { $this_cat = $this_category->parent; } ?>
-
-
-<?php // Check whether this category's parent has children
+	<?php // Check whether this category's parent has children
 	$term = get_queried_object();
 	$children = get_terms( $term->taxonomy, array(
-	'parent'    => $term->term_id,
-	'hide_empty' => false
-	) );
+		'parent'    => $term->term_id,
+		'hide_empty' => false
+		)
+	);
 	// print_r($children); // uncomment to examine for debugging
 	if($children) { // get_terms will return false if tax does not exist or term wasn't found. ?>
-	<h2>In this section</h2>
-		<ul class='child-menu'>
-			<?php wp_list_categories('orderby=slug&order=ASC&show_option_none=&title_li=&child_of=' . $this_cat . ''); ?>
-				</ul>
-		<?php } ?>
-
+		<h2>In this section</h2>
+			<ul class='child-menu'>
+				<?php wp_list_categories('orderby=slug&order=ASC&show_option_none=&title_li=&child_of=' . $this_cat . ''); ?>
+			</ul>
 	<?php } ?>
-
-
-
-
-        
-        
-
+<?php } ?>
 
 <?php if ( is_tax('') || is_singular('local_services') ) { } else { ?>
-<?php if ( is_archive('') ) { ?>
-	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Posts')) : endif; ?>
-		<?php } } ?>
-
+	<?php if ( is_archive('') ) { ?>
+		<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Posts')) : endif; ?>
+	<?php } ?>
+<?php } ?>
 
 <?php if ( is_tax('') || is_singular('local_services') ) { } else { ?>
-<?php if ( is_single('') ) { ?>
-	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Single Post')) : endif; ?>
-		<?php } } ?>
-		
-        
-        
-        
-        
-        
-        
-        
-   
+	<?php if ( is_single('') ) { ?>
+		<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Single Post')) : endif; ?>
+	<?php } ?>
+<?php } ?>
