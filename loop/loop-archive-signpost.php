e105ca2d626a41be5a162b22074cdc54a71465c2
dtw <dtw@users.github.com>
2019-06-08 23:11:58 +0100

    Whitespace

--- loop/loop-archive-signpost.php
+++ loop/loop-archive-signpost.php
@@ -12,28 +12,28 @@
 
 
 
-    <div style="display: flex; justify-content:space-between; align-items:flex-end;">    
+    <div style="display: flex; justify-content:space-between; align-items:flex-end;">
         <h2><?php the_title(); ?>
 
-        
-       <?php if (is_tag()) { 
-         echo "<small>&laquo; "; 
+
+       <?php if (is_tag()) {
+         echo "<small>&laquo; ";
 
 
 
 
 	$taxonomy = 'category';
- 
+
 // Get the term IDs assigned to post.
 $post_terms = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' => 'ids' ) );
- 
+
 // Separator between links.
 $separator = ', ';
- 
+
 if ( ! empty( $post_terms ) && ! is_wp_error( $post_terms ) ) {
- 
+
     $term_ids = implode( ',' , $post_terms );
- 
+
     $terms = wp_list_categories( array(
         'title_li' => '',
 		'depth' => 1,
@@ -42,32 +42,32 @@ if ( ! empty( $post_terms ) && ! is_wp_error( $post_terms ) ) {
         'taxonomy' => $taxonomy,
         'include'  => $term_ids
     ) );
- 
+
     $terms = rtrim( trim( str_replace( '<br />',  $separator, $terms ) ), $separator );
- 
+
     // Display post categories.
     echo  $terms;
-	
+
 }
 
 
 
 
 
-			}  
-	echo "</small>"; 
+			}
+	echo "</small>";
 			?>
 
 
 
 
         </h2>
-        <p style="margin-bottom: 20px;"><?php edit_post_link(); ?></p> 
-        </div>   
+        <p style="margin-bottom: 20px;"><?php edit_post_link(); ?></p>
+        </div>
 	        <?php the_content(); ?>
 
 
-			<?php // get_template_part('elements/post-meta'); ?>        				
+			<?php // get_template_part('elements/post-meta'); ?>
 
 
 		<?php the_tags( '<p><small>Topics: ', ', ', '</small></p>' ); ?>
@@ -94,11 +94,11 @@ if ( ! empty( $post_terms ) && ! is_wp_error( $post_terms ) ) {
 			</article>
 
 	<?php endwhile; ?>
-	
-			<?php get_template_part('elements/pagination'); ?>    
-    
+
+			<?php get_template_part('elements/pagination'); ?>
+
 	<?php else: ?>
 		<p>Sorry, nothing found.</p>
 	<?php endif; ?>
-	
+
  </div><!-- end of Loop Archive -->
