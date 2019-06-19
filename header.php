53ccaa13df441ad1d9f2c20cf1793907dd470e3c
dtw <dtw@users.noreply.github.com>
2019-06-19 12:40:44 +0100

    Remove old font awesome

--- header.php
+++ header.php
@@ -15,9 +15,6 @@
     }
     ?>
 
-<!--<script src="https://use.fontawesome.com/bba45a4e25.js"></script>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">-->
-
 <!-- META TAGS -->
 
 	<?php if (is_home('')) {
