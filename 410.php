06adc531c4124957d0f17c3fb4419d27bf3f57b8
dtw <dtw@users.github.com>
2019-07-04 10:56:15 +0100

    Create 410.php

---
+++ 410.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="container-fluid">
+  <div class="row page-header">
+    <div class="container">
+      <div class="row">
+        <!-- HEADING -->
+        <h1>Sorry, this page has been permenantly removed</h1>
+        <!-- DESCRIPTION -->
+        <p>The page you are looking for has been removed or expired.</p>
+        <p><a href="https://www.healthwatchbucks.co.uk"><i class="fa fa-lg fa-home"></i> Go to the home page</a></p>
+      </div>
+    </div>
+
+</div><!-- end of container-fluid -->
+<?php get_footer(); ?>
