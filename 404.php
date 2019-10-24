d217f7a151bb34f52162bc5fb97ab660e0235048
dtw <dtw@users.github.com>
2019-10-24 11:35:44 +0100

    Add better 404 message

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
-<?php get_header(); ?>
-
-        	</div><!-- end of container-fluid -->
-
-
-
-
+<?php get_header();?>
+<?php global $wp;
+$missing_page_url = home_url( $wp->request );?>
+<div class="container">
+  <p>The page you are looking for does not exist or has been removed permenantly. If you typed in the page address, please check that it's spelt correctly and try again.</p>
+  <p>You can also <a href="mailto:info@healthwatchbucks.co.uk?subject=Missing web page&body=The page at <?php echo $missing_page_url?> returns a 404 error.">inform us about a missing page by email</a>.</p>
+  <p><a href="<?php echo esc_url( home_url() ) ?>"><i class="fa fa-lg fa-home"></i> Go to the home page</a></p>
+</div>
+</div><!-- end of container-fluid -->
 <?php get_footer(); ?>
