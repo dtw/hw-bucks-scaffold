a183bc9b7b229b7191a73620ddb8fa0d36b36073
dtw <dtw@users.github.com>
2019-10-30 16:45:51 +0000

    Add ids and classes

--- footer.php
+++ footer.php
@@ -68,7 +68,7 @@
 
 	<!-- Footer WIDGET area #1 -->
 
-<div class="col-md-4 col-sm-4 col-xs-12" style="padding-left: 0;">
+<div id="footer1" class="footer col-md-4 col-sm-4 col-xs-12">
 	<?php if ( is_active_sidebar( 'Footer_1' ) ) { dynamic_sidebar( 'Footer 1' ); } else { ?>
     <h2 class='text-danger'>Footer area 1 is empty</h2><p>Note to administrator: this area is editable via the <strong>WordPress Dashboard > Appearance > Widgets</strong></p>
     	<?php } ?>
@@ -78,10 +78,10 @@
 
 		<?php // Check whether Footer area #3 has any widgets 
         	if ( is_active_sidebar( 'footer_3' ) ) { ?>
-                  <div class="col-md-4 col-sm-4 col-xs-12">
+                  <div id="footer2" class="footer col-md-4 col-sm-4 col-xs-12">
 
 		<?php } else { // If not, Footer area #2 takes up 2 columns instead of 1 ?>
-			  <div class="col-md-8 col-sm-8 col-xs-12">
+			  <div id="footer2" class="footer col-md-8 col-sm-8 col-xs-12">
 		<?php } ?>
 		
 			<?php if ( is_active_sidebar( 'footer_2' ) ) { dynamic_sidebar( 'Footer 2' ); } else { ?>
@@ -94,7 +94,7 @@
 
 		<?php // Check whether Footer area #3 has any widgets 
         	if ( is_active_sidebar( 'footer_3' ) ) { ?>
-				<div class="col-md-4 col-sm-4 col-xs-12">
+				<div id="footer3" class="footer col-md-4 col-sm-4 col-xs-12">
 			<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 3')) : endif; ?>
 					</div>
 
@@ -140,12 +140,12 @@
    
     <div class="row">
         
-        <div class="col-md-6 col-sm-6 col-xs-12">
+        <div id="copyright" class="col-md-6 col-sm-6 col-xs-12">
             <p>&copy; <strong><?php bloginfo('name'); ?></strong> <?php echo date('Y'); ?></p>
                 </div><!-- end of col 1 -->
         
 
-        <div class="col-md-6 col-sm-6 col-xs-12">
+        <div id="author" class="col-md-6 col-sm-6 col-xs-12">
         <?php $theme_author_url = 'https://www.kingjason.co.uk/wordpress-theme-plugin-healthwatch/'; ?>
             <p class="text-right">WordPress website built by <a href="<?php echo $theme_author_url; ?>" target="_blank" title="Non-profit web design by Jason King"><strong>Jason King</strong></a></p>
                 </div><!-- end of col 2 -->
