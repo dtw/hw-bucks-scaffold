b3e4a0b344b37daf8e2d1d2819fe73ec4d231d8b
dtw <dtw@users.github.com>
2019-11-11 11:32:26 +0000

    Add functions-json-ld.php to theme header

--- header.php
+++ header.php
@@ -5,6 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <?php include('functions/functions-json-ld.php'); ?>
+    <script type="application/ld+json">
+    // <![CDATA[
+    <?php echo json_encode($payload); ?>
+    // ]]
+    </script>
+
     <?php if(is_home()) {
       echo '
       <!-- add disable brower cache on frontpage -->
