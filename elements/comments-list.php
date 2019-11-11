2375c84222b1feea6bf4c7c2d9c7cfee4b4b0fc9
dtw <dtw@users.github.com>
2019-11-11 13:49:53 +0000

    Whitespace efforts

--- elements/comments-list.php
+++ elements/comments-list.php
@@ -7,34 +7,25 @@ $args = array(
 	);
 
 $comments = get_comments($args); ?>
+<?php if ( is_singular('local_services') ) {
+	if ($comments) { ?>
+		<hr /><h2>Service users have provided the following ratings and reviews</h2>
+		<p><small>If <?php the_title(); ?> would like to respond to any of the comments, email <a href="mailto:info@healthwatchbucks.co.uk">info@healthwatchbucks.co.uk</a></small></p>
+<?php }
+	}
+?>
 
-<?php if ( is_singular('local_services') ) { ?>
-<?php if ($comments) { ?>
-<hr /><h2>Service users have provided the following ratings and reviews</h2>
-<p><small>If <?php the_title(); ?> would like to respond to any of the comments, email <a href="mailto:info@healthwatchbucks.co.uk">info@healthwatchbucks.co.uk</a></small></p>
-
-<?php } } ?>
-
-<?php foreach($comments as $comment) { ?>
-
-<?php // Check whether approved
-$comment_id = '';
-$status = wp_get_comment_status( $comment_id );
-if ( $status == "approved" ) { ?>
-
-
-
-    <div class="comment">
-
-
-	<?php $individual_rating = get_comment_meta( $comment->comment_ID, 'feedback_rating', true ); ?>
-
-<?php if ($individual_rating) { $star_count = 0; ?>
-
-
-
-    <p class="star-rating p-rating">
-			<?php
+<?php foreach($comments as $comment) {
+	// Check whether approved
+	$comment_id = '';
+	$status = wp_get_comment_status( $comment_id );
+	if ( $status == "approved" ) { ?>
+		<div class="comment">
+			<?php $individual_rating = get_comment_meta( $comment->comment_ID, 'feedback_rating', true );
+			if ($individual_rating) {
+				$star_count = 0; ?>
+				<p class="star-rating p-rating">
+					<?php
 				for ($int_count = 1; $int_count <= floor($individual_rating); $int_count++) {
 					echo '<i class="fa fa-star fa-lg"></i>
 					';
@@ -52,8 +43,7 @@ if ( $status == "approved" ) { ?>
 
 	<?php } // end of if there is a rating ?>
 
-		<blockquote class="e-description"><?php comment_text(); ?>
-</blockquote>
+		<blockquote class="e-description"><?php comment_text(); ?></blockquote>
 
 
 
