8d42fdf51c072c3354356407244329f08c548515
dtw <dtw@users.github.com>
2020-01-18 16:06:50 +0000

    Fix pagination on small screens

--- elements/pagination.php
+++ elements/pagination.php
@@ -1,7 +1,13 @@
 <!-- WP Paginate plugin should be installed to make this work properly. In the plugin settings, choose to put pagination in .well or .jumbotron -->
 
 <?php if(function_exists('wp_paginate')) {
-    wp_paginate();
+  echo '<div class="paginate-container col-sm-12 hidden-xs">';
+    wp_paginate('');
+  echo '</div>';
+  // we need different settings on mobile
+  echo '<div class="paginate-container hidden-lg hidden-md hidden-sm col-xs-12">';
+    wp_paginate('range=1&anchor=1&gap=0');
+  echo '</div>';
 }
 else { ?>
 
