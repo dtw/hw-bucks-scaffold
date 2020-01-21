149049574532d0edaf0eb3e4064a594c464c55a5
dtw <dtw@users.noreply.github.com>
2020-01-21 14:47:55 +0000

    FIx pagination
    WIll it work now?!

--- elements/pagination.php
+++ elements/pagination.php
@@ -1,13 +1,9 @@
 <!-- WP Paginate plugin should be installed to make this work properly. In the plugin settings, choose to put pagination in .well or .jumbotron -->
 
 <?php if(function_exists('wp_paginate')) {
-  echo '<div class="paginate-container col-sm-12 hidden-xs">';
-    wp_paginate('');
-  echo '</div>';
+  wp_paginate('before=<div class="paginate-container col-sm-12 hidden-xs">&after=</div>');
   // we need different settings on mobile
-  echo '<!-- mobile wp-paginate --><div class="paginate-container hidden-lg hidden-md hidden-sm col-xs-12">';
-    wp_paginate('title=<div class="paginate-title col-xs-12"><span class="title">Pages:</span></div>&before=<div class="paginate-pages col-xs-12">&after=</div>&range=1&anchor=1&gap=0');
-  echo '</div><!-- end mobile wp-paginate-->';
+  wp_paginate('before=<!--before--><div class="paginate-container hidden-lg hidden-md hidden-sm col-xs-12">&after=<!--after--></div>&range=1&anchor=1&gap=0');
 }
 else { ?>
 
