a90d01dd35b194f5b7e873aad29c12971f8ef2f4
dtw <dtw@users.github.com>
2020-01-31 20:04:33 +0000

    Remove hr

--- loop/loop-single-local_services.php
+++ loop/loop-single-local_services.php
@@ -98,7 +98,7 @@
 
 		<?php if ( get_post_meta( $id, 'hw_services_date_visited', true ) )  { ?>
 
-						<hr /><h2>Healthwatch Bucks has visited, rated and reviewed <?php the_title(); ?></h2>
+						<h2>Healthwatch Bucks has visited, rated and reviewed <?php the_title(); ?></h2>
 
 			<p><i class="fas fa-calendar service-icon" aria-hidden="true" title="Date"></i> Visited <strong><?php echo get_post_meta( $id, 'hw_services_date_visited', true ); ?></strong><br />
 							</p>
