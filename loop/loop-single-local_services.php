92ed0ff1e994d725692310cb9f1903b907d42823
dtw <dtw@users.github.com>
2020-02-01 09:20:01 +0000

    Removed service div and row

--- loop/loop-single-local_services.php
+++ loop/loop-single-local_services.php
@@ -1,94 +1,93 @@
 <div class="loop-single-service">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<div class="row">
-			<div id="service" class="col-md-12 col-sm-8 col-xs-12">
-				<div class="row">
-					<div id="service-contact-details" class="col-md-4 col-sm-12 col-xs-12">
-						<p>
-							<span class="screen-reader-text">Street address</span>
-							<?php if ( get_post_meta( $post->ID, 'hw_services_address_line_1', true ) )  { ?>
-								<?php echo get_post_meta( $post->ID, 'hw_services_address_line_1', true ); ?><br />
-									<?php } ?>
+			<div id="service-contact-details" class="col-md-4 col-sm-6 col-xs-12">
+				<p>
+					<span class="screen-reader-text">Street address</span>
+					<?php if ( get_post_meta( $post->ID, 'hw_services_address_line_1', true ) )  { ?>
+						<?php echo get_post_meta( $post->ID, 'hw_services_address_line_1', true ); ?><br />
+							<?php } ?>
 
-							<?php if ( get_post_meta( $post->ID, 'hw_services_address_line_2', true ) )  { ?>
-								<?php echo get_post_meta( $post->ID, 'hw_services_address_line_2', true ); ?><br />
-									<?php } ?>
+					<?php if ( get_post_meta( $post->ID, 'hw_services_address_line_2', true ) )  { ?>
+						<?php echo get_post_meta( $post->ID, 'hw_services_address_line_2', true ); ?><br />
+							<?php } ?>
 
-							<?php if ( get_post_meta( $post->ID, 'hw_services_city', true ) )  { ?>
-								<?php echo get_post_meta( $post->ID, 'hw_services_city', true ); ?><br />
-									<?php } ?>
+					<?php if ( get_post_meta( $post->ID, 'hw_services_city', true ) )  { ?>
+						<?php echo get_post_meta( $post->ID, 'hw_services_city', true ); ?><br />
+							<?php } ?>
 
-							<?php if ( get_post_meta( $post->ID, 'hw_services_county', true ) )  { ?>
-								<?php echo get_post_meta( $post->ID, 'hw_services_county', true ); ?><br />
-									<?php } ?>
+					<?php if ( get_post_meta( $post->ID, 'hw_services_county', true ) )  { ?>
+						<?php echo get_post_meta( $post->ID, 'hw_services_county', true ); ?><br />
+							<?php } ?>
 
-							<?php if ( get_post_meta( $post->ID, 'hw_services_postcode', true ) )  { ?>
-								<?php echo get_post_meta( $post->ID, 'hw_services_postcode', true ); ?><br />
-									<?php } ?>
-						</p>
-						<?php if ( get_post_meta( $post->ID, 'hw_services_phone', true ) )  {
-							$hw_services_phone_tmp = get_post_meta( $post->ID, 'hw_services_phone', true );
-							$hw_services_phone_tmp = str_replace(' ', '', $hw_services_phone_tmp);
-							$mask = "%s%s%s%s%s %s%s%s %s%s%s";
-							$hw_services_phone_tmp = vsprintf($mask, str_split($hw_services_phone_tmp));
-							?>
-						<p><i class="fas fa-phone service-icon" aria-hidden="true" title="Telephone"></i>
-							<strong><?php echo $hw_services_phone_tmp; ?></strong><br />
-						</p>
-						<?php } ?>
-						<?php if ( get_post_meta( $post->ID, 'hw_services_website', true ) )  { ?>
-						<p><i class="fas fa-external-link-alt service-icon" aria-hidden="true" title="Website"></i>
-							<a target="_blank"  href="<?php echo esc_url(get_post_meta( $post->ID, 'hw_services_website', true )); ?>" title="<?php the_title(); ?>" >Visit their website &raquo;</a><br />
-						</p>
-						<?php } ?>
-					</div><!-- end of col 1 service-contact-details -->
-					<div id="service-rating-details" class="col-md-7 col-sm-12 col-xs-12 col-md-offset-1">
-						<?php if ( get_post_meta( $post->ID, 'hw_services_overall_rating', true ) )  { ?>
-						<div id="overall-rating" class="row">
-							<div class="col-md-6 col-sm-12 col-xs-12">
-								<p><strong>Our overall rating:</strong></p>
-							</div><!-- end of col -->
-							<div class="col-md-6 col-sm-12 col-xs-12">
-								<p>
-								<?php $rating = get_post_meta( $post->ID, 'hw_services_overall_rating', true );
-									echo feedbackstarrating($rating,array('colour' => 'green','size' => 'fa-lg'));
-									if ($rating == 1) echo '<span class="screen-reader-text">'.$rating.' star</span>';
-									else echo '<span class="screen-reader-text">'.$rating.' stars</span>';
-								?>
-								</p>
-							</div><!-- end of col -->
-						</div><!-- end of row -->
-						<?php } ?>
-						<?php // QUERY the COMMENTS for current single post
-						$args = array (
-							'post_id' => $post->ID,
-							'status' => 'approve'
-							);
-
-						$comments = get_comments($args);
-						?>
-
-						<?php if ( $comments ) { ?>
-
-						<div id="public-rating" class="row">
-							<div class="col-md-6 col-sm-12 col-xs-12">
-								<p><strong>Public rating:</strong></p>
-							</div><!-- end of col -->
-							<div class="col-md-6 col-sm-12 col-xs-12">
-								<?php get_template_part('elements/comments-rating-average'); ?>
-							</div><!-- end of col -->
-						</div><!-- end of row -->
-						<?php } else { ?>
-						<p><a href="<?php echo get_the_permalink(); ?>#respond">Leave your own rating &raquo;</a></p>
-						<?php } ?>
-					</div><!-- end of col 2 service-rating-details -->
-				</div><!-- end of row -->
-			</div><!-- end of service -->
+					<?php if ( get_post_meta( $post->ID, 'hw_services_postcode', true ) )  { ?>
+						<?php echo get_post_meta( $post->ID, 'hw_services_postcode', true ); ?><br />
+							<?php } ?>
+				</p>
+				<?php if ( get_post_meta( $post->ID, 'hw_services_phone', true ) )  {
+					$hw_services_phone_tmp = get_post_meta( $post->ID, 'hw_services_phone', true );
+					$hw_services_phone_tmp = str_replace(' ', '', $hw_services_phone_tmp);
+					$mask = "%s%s%s%s%s %s%s%s %s%s%s";
+					$hw_services_phone_tmp = vsprintf($mask, str_split($hw_services_phone_tmp));
+					?>
+				<p><i class="fas fa-phone service-icon" aria-hidden="true" title="Telephone"></i>
+					<strong><?php echo $hw_services_phone_tmp; ?></strong><br />
+				</p>
+				<?php } ?>
+				<?php if ( get_post_meta( $post->ID, 'hw_services_website', true ) )  { ?>
+				<p><i class="fas fa-external-link-alt service-icon" aria-hidden="true" title="Website"></i>
+					<a target="_blank"  href="<?php echo esc_url(get_post_meta( $post->ID, 'hw_services_website', true )); ?>" title="<?php the_title(); ?>" >Visit their website</a><br />
+				</p>
+				<?php } ?>
+			</div><!-- end of col 1 service-contact-details -->
 			<?php if ( has_post_thumbnail() ) { ?>
-			<div id="thumbnail-container-body" class="hidden-lg hidden-md col-sm-4 col-xs-12 text-center sidebar-container">
+			<div id="thumbnail-container-body" class="hidden-lg hidden-md col-sm-6 col-xs-12 text-center sidebar-container">
 				<?php echo the_post_thumbnail('medium'); ?>
 			</div>
 			<?php } ?>
+			<div id="service-rating-details" class="col-md-7 col-sm-10 col-xs-12 col-md-offset-1 col-sm-offset-1">
+				<?php if ( get_post_meta( $post->ID, 'hw_services_overall_rating', true ) )  { ?>
+				<div id="overall-rating" class="row">
+					<div class="col-md-6 col-sm-4 col-xs-12">
+						<p><strong>Our overall rating:</strong></p>
+					</div><!-- end of col -->
+					<div class="col-md-6 col-sm-8 col-xs-12">
+						<p>
+						<?php $rating = get_post_meta( $post->ID, 'hw_services_overall_rating', true );
+							echo feedbackstarrating($rating,array('colour' => 'green','size' => 'fa-lg'));
+							if ($rating == 1) echo '<span class="screen-reader-text">'.$rating.' star</span>';
+							else echo '<span class="screen-reader-text">'.$rating.' stars</span>';
+						?>
+						</p>
+					</div><!-- end of col -->
+				</div><!-- end of row -->
+				<?php } ?>
+				<?php // QUERY the COMMENTS for current single post
+				$args = array (
+					'post_id' => $post->ID,
+					'status' => 'approve'
+					);
+
+				$comments = get_comments($args);
+				?>
+
+				<?php if ( $comments ) { ?>
+
+				<div id="public-rating" class="row">
+					<div class="col-md-6 col-sm-4 col-xs-12">
+						<p><strong>Public rating:</strong></p>
+					</div><!-- end of col -->
+					<div class="col-md-6 col-sm-8 col-xs-12">
+						<?php get_template_part('elements/comments-rating-average'); ?>
+					</div><!-- end of col -->
+				</div><!-- end of row -->
+			<?php } ?>
+				<div id="rating-link" class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12 text-right">
+						<p><a href="<?php echo get_the_permalink(); ?>#respond">Leave your own rating &raquo;</a></p>
+					</div>
+				</div>
+			</div><!-- end of col 2 service-rating-details -->
 		</div><!-- end of row -->
 
 
