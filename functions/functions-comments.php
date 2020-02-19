dcfaa77fab76ed2606d0843256c4ce821bafe6de
dtw <dtw@users.noreply.github.com>
2020-02-19 15:15:49 +0000

    Fix redirect to thanks on comments

--- functions/functions-comments.php
+++ functions/functions-comments.php
@@ -1,15 +1,16 @@
 <?php
 
-// Redirect to thank you post after comment
+// Redirect to thank you post after comment on local_services
 
-//if (is_singular('local_services')) {
+add_action('comment_post_redirect', 'redirect_to_thank_page', 10,2 );
 
-add_action('comment_post_redirect', 'redirect_to_thank_page');
-
-function redirect_to_thank_page() {
+function redirect_to_thank_page( $location, $commentdata) {
 	$the_url = site_url();
-		return $the_url . '/thanks/';
+  $post_id = $commentdata->comment_post_ID;
+  if('local_services' == get_post_type($post_id)){
+    return $the_url . '/thanks/';
+  }
+  return $location;
 	}
 
-// 		}
 ?>
