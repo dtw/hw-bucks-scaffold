ed2e9ad52696c89ce089bf7de79ddc6ac1c5d105
dtw <362017+dtw@users.noreply.github.com>
2022-03-18 12:56:35 +0000

    Add date to single posts

--- loop/loop-default.php
+++ loop/loop-default.php
@@ -10,7 +10,30 @@
 
 	<?php the_content(); ?>
 
-		<?php the_tags( "<div class='the-tags'><span class='screen-reader-text'>This post's tags</span><p><span aria-hidden='true'>Explore more </span>", " ", "</p></div>" ); ?>
+	<?php //if Post show the date
+	if ( is_single() ) { ?>
+		<div class="the-date">
+			<p><i class="fa fa-clock-o" aria-hidden="true"></i>
+				<?php
+				$published_date = get_post_datetime($post,'date');
+				$mod_date = get_post_datetime($post,'modified');
+				$interval = $published_date->diff($mod_date);
+				// if it is in the current year don't show the year
+				if ($published_date->format('Y') == current_datetime()->format('Y')) {
+					$date_str_format = 'j M';
+				} else {
+					$date_str_format = 'j M, Y';
+				}
+				// if the interval is less than 2 days, just show pub date
+				if ($interval->d <= 2) {
+					echo " Published on " . get_the_date($date_str_format);
+				} else {
+					echo " Published on " . get_the_date($date_str_format) . " (updated " . get_the_modified_date($date_str_format).")";
+		    }?>
+			</p>
+		</div>
+	<?php } ?>
+	<?php the_tags( "<div class='the-tags'><span class='screen-reader-text'>This post's tags</span><p><span aria-hidden='true'>Explore more </span>", " ", "</p></div>" ); ?>
 
 	<?php endwhile; ?>
 
