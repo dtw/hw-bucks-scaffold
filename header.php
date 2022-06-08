1895e52f4a02d58c6355edd1182c764ea139f9f5
Phil T <362017+dtw@users.noreply.github.com>
2022-06-08 10:50:06 +0100

    Move code to elements/header-head

--- header.php
+++ header.php
@@ -1,43 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <?php include('functions/functions-json-ld.php'); ?>
-    <script type="application/ld+json">
-    // <![CDATA[
-    <?php echo json_encode($payload); ?>
-    // ]]
-    </script>
-
-    <?php if(is_home()) {
-      echo '
-      <!-- add disable brower cache on frontpage -->
-      <meta http-equiv="Pragma" content="no-cache" />
-      <meta http-equiv="Expires" content="0" />
-      ';
-    }
-    ?>
-
-<!-- SUPPORT FOR INTERNET EXPLORER -->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-
-<!-- SHOW / HIDE NAV MENU ON SCROLL -->
-
-<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-
-    <?php wp_head(); ?>
-	  </head>
-
+	<?php get_template_part('elements/header-head'); ?>
 
 <?php // BODY class for ANCESTOR pages
 	/* Get the Page Slug to Use as a Body Class, this will only return a value on pages */
