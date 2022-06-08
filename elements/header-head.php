2bb8eb6497636a4db5a6da06f77a18cdcd62addb
Phil T <362017+dtw@users.noreply.github.com>
2022-06-08 13:08:40 +0100

    Fix file path

--- elements/header-head.php
+++ elements/header-head.php
@@ -3,7 +3,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <?php include('functions/functions-json-ld.php'); ?>
+  <?php include(get_template_directory().'/functions/functions-json-ld.php'); ?>
   <script type="application/ld+json">
   // <![CDATA[
   <?php echo json_encode($payload); ?>
