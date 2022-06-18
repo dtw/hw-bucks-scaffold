099d36781835faa30696a577788e403266eb30b4
Phil T <362017+dtw@users.noreply.github.com>
2022-06-18 09:07:36 +0100

    Output Socials from theme customiser

--- widgets/widget-social-block.php
+++ widgets/widget-social-block.php
@@ -33,11 +33,27 @@ class scaffold_social_block extends WP_Widget {
     $widget_option   = $instance['widget_option']; // Text of the link
 
     // Output
-		echo $before_widget;
-		echo '<p>' . $widget_option . '</p>';
-		// echo widget closing tag
-		echo $after_widget;
-	}
+    echo $before_widget;
+    echo '<div class="social-icons">';
+      if ( get_theme_mod( 'scaffold_org_facebook') ) {
+        echo '<a href="https://www.facebook.com/' . get_theme_mod( 'scaffold_org_facebook') . '/" aria-label="Facebook"><i aria-hidden="true" class="fab fa-2x fa-facebook"></i></a>';
+      }
+      if ( get_theme_mod( 'scaffold_org_twitter') ) {
+        echo '<a href="https://twitter.com/' . get_theme_mod( 'scaffold_org_twitter') . '" aria-label="Twitter"><i aria-hidden="true" class="fab fa-2x fa-twitter-square"></i></a>';
+      }
+      if ( get_theme_mod( 'scaffold_org_linkedin') ) {
+        echo '<a href="https://www.linkedin.com/company/' . get_theme_mod( 'scaffold_org_linkedin') . '" aria-label="LinkedIn"><i aria-hidden="true" class="fab fa-2x fa-linkedin"></i></a>';
+      }
+      if ( get_theme_mod( 'scaffold_org_youtube') ) {
+        echo '<a href="https://www.youtube.com/channel/' . get_theme_mod( 'scaffold_org_youtube') . '" aria-label="YouTube"><i aria-hidden="true" class="fab fa-2x fa-youtube-square"></i></a>';
+      }
+      if ( get_theme_mod( 'scaffold_org_rss') ) {
+        echo '<a href="' . get_bloginfo('rss2_url') . '" aria-label="RSS feed"><i aria-hidden="true" class="fa fa-2x fa-rss"></i></a>';
+      }
+    echo '</div>';
+    // echo widget closing tag
+    echo $after_widget;
+  }
 
   /** Widget control update */
   function update( $new_instance, $old_instance ) {
