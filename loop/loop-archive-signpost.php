ad5867852c72836e7b4f54f1dda0934a0b9c36fc
Phil T <362017+dtw@users.noreply.github.com>
2022-06-24 09:09:51 +0100

    Move inline styles to CSS

--- loop/loop-archive-signpost.php
+++ loop/loop-archive-signpost.php
@@ -3,7 +3,7 @@
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <article>
-      <div style="display: flex; justify-content:space-between;">
+      <div id="archive-signpost-title">
         <h2>
           <?php the_title(); ?>
         </h2>
