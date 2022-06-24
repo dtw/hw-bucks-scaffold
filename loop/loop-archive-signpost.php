1238436b903b5cdb8b843f74e08ae34cbd17f53e
Phil T <362017+dtw@users.noreply.github.com>
2022-06-24 10:21:51 +0100

    Switch ID to class

--- loop/loop-archive-signpost.php
+++ loop/loop-archive-signpost.php
@@ -3,7 +3,7 @@
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <article>
-      <div id="archive-signpost-title">
+      <div class="archive-signpost-title">
         <h2>
           <?php the_title(); ?>
         </h2>
