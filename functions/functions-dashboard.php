4e02271a3af1b01b456c0c4d5fa9266da69315ab
Phil T <362017+dtw@users.noreply.github.com>
2022-06-29 20:43:38 +0100

    Use site icon if set else fallback to builtin

--- functions/functions-dashboard.php
+++ functions/functions-dashboard.php
@@ -2,16 +2,9 @@
 
 // Custom Admin Login Logo
 function custom_login_logo() {
-	echo '<style type="text/css">
-	h1 a { background-image: url('.get_template_directory_uri().'/images/login.png) !important; }
-	</style>';
+	$logo_style = '<style type="text/css">h1 a { background-image: url(' . get_site_icon_url( 512, get_template_directory_uri().'/images/login.png') . ') !important; }</style>';
+  echo $logo_style;
 }
 add_action('login_head', 'custom_login_logo');
-
-
-
-
-
-
 
 ?>
