00d493c0bf298d96b8ef70fd9b4de2d4ce779544
Phil T <362017+dtw@users.noreply.github.com>
2022-06-29 20:43:15 +0100

    Move to functions

--- functions.php
+++ functions.php
@@ -91,6 +91,8 @@ update_option('image_default_link_type','none');
 function remove_meta_boxes() {
 	remove_meta_box( 'trackbacksdiv' , 'post' , 'normal' );
 	remove_meta_box( 'trackbacksdiv' , 'page' , 'normal' );
+	remove_meta_box( 'commentsdiv' , 'post' , 'normal');
+	remove_meta_box( 'commentsdiv' , 'page' , 'normal');
 }
 add_action( 'admin_menu' , 'remove_meta_boxes' );
 
--- functions/functions-dashboard.php
+++ functions/functions-dashboard.php
@@ -1,20 +1,5 @@
 <?php
 
-
-
-
-
-// Hide meta boxes from everyone
-function my_remove_meta_boxes() {
- if( !current_user_can('manage_options') ) {
-	remove_meta_box('commentsdiv', 'post', 'normal');
- }
-}
-add_action( 'admin_menu', 'my_remove_meta_boxes' );
-
-
-
-
 // Custom Admin Login Logo
 function custom_login_logo() {
 	echo '<style type="text/css">
