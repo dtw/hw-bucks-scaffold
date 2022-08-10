c6c7e81fd414613493261b9cc717f3b6bc41a441
Phil T <362017+dtw@users.noreply.github.com>
2022-08-10 18:21:51 +0100

    Whitespace

--- elements/comments-rating-average.php
+++ elements/comments-rating-average.php
@@ -1,48 +1,27 @@
 <div class="star-rating">
 
 <?php
-
 // returns an array containing the
 $rating = hw_feedback_get_rating($post);
 
 if ($rating['count'] == 0) {
-
 	if ($gadget <> "yes") {
-
-	if  ( is_tax() || is_page_template() ) { ?>
-   <p><a href="<?php echo the_permalink(); ?>"><i class="fas fa-comments" aria-hidden="true"></i> Give feedback</a></p>
-		<?php } ?>
-
-
-<?php	} } else {
-
-?>
-
-
-
-<?php $average_rating = $rating['average'];
+		if  ( is_tax() || is_page_template() ) { ?>
+			<p><a href="<?php echo the_permalink(); ?>"><i class="fas fa-comments" aria-hidden="true"></i> Give feedback</a></p>
+		<?php }
+	}
+} else {
+	$average_rating = $rating['average'];
 	$average_rating = round($average_rating,1);
-	?>
-
-
-
-<?php if ( $gadget == "yes" && $rating['average'] < 3 ) { } else { ?>
-
-<p>
-	<?php
-		echo hw_feedback_star_rating($average_rating,array('size' => 'fa-lg'));
-	?>
-</p>
-<p>Rated <strong><?php echo $average_rating; ?></strong> out of 5 by <strong><?php echo $rating['count']; ?>
-<?php if ($rating['count'] <= 1) { echo " person"; } else { echo " people"; } ?>
-</strong></p>
-
-
-<?php } ?>
-
-
-
-<?php } // end IF ?>
+	if ( $gadget == "yes" && $rating['average'] < 3 ) { } else { ?>
+		<p>
+			<?php echo hw_feedback_star_rating($average_rating,array('size' => 'fa-lg'));	?>
+		</p>
+		<p>Rated <strong><?php echo $average_rating; ?></strong> out of 5 by <strong>
+			<?php echo $rating['count']; if ($rating['count'] <= 1) { echo " person"; } else { echo " people"; } ?>
+		</strong></p>
+	<?php }
+} // end count IF ?>
 
 <p><a href="<?php echo get_the_permalink(); ?>#response-header">Leave your own rating &raquo;</a></p>
 
