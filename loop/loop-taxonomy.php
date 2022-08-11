7af6c51f12ee980faaabcb7d8be2140f52fe75b8
Phil T <362017+dtw@users.noreply.github.com>
2022-08-11 15:11:52 +0100

    Whitespace

--- loop/loop-taxonomy.php
+++ loop/loop-taxonomy.php
@@ -17,95 +17,56 @@
 		?>
 		<!-- Start content for service -->
 		<div class="col-md-2 col-sm-3 hidden-xs">
+			<?php														// Display icon for taxonomy term
 
-
-<?php														// Display icon for taxonomy term
-
-if (is_tax('service_types')) 	{ 												// If this is a taxonomy page
-	$term_id = get_queried_object_id();							// Get taxonomy ID
+			if (is_tax('service_types')) 	{ 												// If this is a taxonomy page
+				$term_id = get_queried_object_id();							// Get taxonomy ID
+			} else 	{ 												// Or if this is a widget
+				$term_ids = get_the_terms( $get_the_ID , 'service_types' );		// Find taxonomies
+				$term_id = $term_ids[0]->term_id;								// Get taxonomy ID
+			}
+			$term_icon = get_term_meta( $term_id, 'icon', true );	// Use it to get icon field
+			?>
+			<a href="<?php the_permalink(); ?>">
+				<img width="100%" src="<?php echo $term_icon; ?>" alt="<?php the_title(); ?>" />
+			</a>
+		</div><!-- end of 1st col -->
+  	<div class="col-md-10 col-sm-9">
+	    <?php if (is_tax('service_types')) {
+				// if tax is a service get the city and add it to the title
+				$city = get_post_meta( $post->ID, 'hw_services_city', true );
+				$title_link_string = ($city) ? get_the_title() . ' ('.$city.')' : get_the_title();
+				echo '<h2><a class="title-link" href="' . get_the_permalink() . '" rel="bookmark">' . $title_link_string . '</a></h2>';
+			} else { ?>
+				<h2><a href="<?php echo the_permalink(''); ?>"><?php the_title(); ?></a></h2>
+			<?php }
+			if ( has_excerpt( $post->ID ) ) {
+				the_excerpt();
+			} ?>
+		</div><!-- end of 2nd col -->
+		<div class="col-md-4 col-sm-4">
+			<?php get_template_part('elements/comments-rating-average'); ?>
+		</div><!-- end of 3rd col -->
+	</div><!-- end of row / service -->
+	<?php endwhile;
+	if(!is_home()) {
+		get_template_part('elements/pagination');
 	}
-
-		else 	{ 												// Or if this is a widget
-	$term_ids = get_the_terms( $get_the_ID , 'service_types' );		// Find taxonomies
-	$term_id = $term_ids[0]->term_id;								// Get taxonomy ID
-	}
-
-    $term_icon = get_term_meta( $term_id, 'icon', true );	// Use it to get icon field
-		?>
-
-	<a href="<?php the_permalink(); ?>">
-    <img width="100%" src="<?php echo $term_icon; ?>" alt="<?php the_title(); ?>" />
-    </a>
-
-            </div><!-- end of 1st col -->
-
-
-        <div class="col-md-10 col-sm-9">
-
-            <?php if (is_tax('service_types')) {
-							// if tax is a service get the city and add it to the title
-							$city = get_post_meta( $post->ID, 'hw_services_city', true );
-							$title_link_string = ($city) ? get_the_title() . ' ('.$city.')' : get_the_title();
-							echo '<h2><a class="title-link" href="' . get_the_permalink() . '" rel="bookmark">' . $title_link_string . '</a></h2>';
-						} else { ?>
-							<h2><a href="<?php echo the_permalink(''); ?>"><?php the_title(); ?></a></h2>
-						<?php }
-
-  if ( has_excerpt( $post->ID ) ) {
-	the_excerpt();
-		} ?>
-
-
-            </div><!-- end of 2nd col -->
-
-
-        <div class="col-md-4 col-sm-4">
-
-
-
-
-		<?php get_template_part('elements/comments-rating-average'); ?>
-
-                </div><!-- end of 3rd col -->
-
-
-
-
-                    </div><!-- end of row / service -->
-
-
-        <?php endwhile; ?>
-
-
-           <?php if(!is_home()) { ?>
-                <?php get_template_part('elements/pagination'); ?>
-       				<?php } ?>
-
-     <?php wp_reset_query(); ?>
-
-<div class="jumbotron">
+	wp_reset_query(); ?>
+	<div class="jumbotron">
 		<p class="text-center">
-        	<i class="fas fa-4x fa-search"></i>
-			</p>
-
-				<h2 class="text-center">Didn't find the local health service you were looking for?</h2>
-                <p class="text-center">You can still <a href="<?php bloginfo('url'); ?>/your-feedback/">send us feedback</a>.</p>
-                </div><!-- end of Jumbotron -->
-
-                <?php else: ?>
-
-            <h2 class="text-center">Sorry, no services listed under <strong><?php single_cat_title(); ?></strong>.</h2>
-
-<div class="jumbotron">
+			<i class="fas fa-4x fa-search"></i>
+		</p>
+		<h2 class="text-center">Didn't find the local health service you were looking for?</h2>
+		<p class="text-center">You can still <a href="<?php bloginfo('url'); ?>/your-feedback/">send us feedback</a>.</p>
+	</div><!-- end of Jumbotron -->
+<?php else: ?>
+	<h2 class="text-center">Sorry, no services listed under <strong><?php single_cat_title(); ?></strong>.</h2>
+	<div class="jumbotron">
 		<p class="text-center">
-        	<i class="fas fa-4x fa-search"></i>
-			</p>
-
-				<p class="text-center"
-                >Didn't find the local health service you were looking for? You can still <a href="<?php bloginfo('url'); ?>/your-feedback/">send us feedback</a>.</p>
-		</div><!-- end of Jumbotron -->
-
-
-        <?php endif; ?>
-
+			<i class="fas fa-4x fa-search"></i>
+		</p>
+		<p class="text-center">Didn't find the local health service you were looking for? You can still <a href="<?php bloginfo('url'); ?>/your-feedback/">send us feedback</a>.</p>
+	</div><!-- end of Jumbotron -->
+<?php endif; ?>
 </div><!-- end of Loop Taxonomy -->
