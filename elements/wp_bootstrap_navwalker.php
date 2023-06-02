c4a0ba29794c5f51f9f5d1765b74c9b279a433f5
Phil T <362017+dtw@users.noreply.github.com>
2023-06-02 15:38:56 +0100

    Remove this weird space
    For now at least...

--- elements/wp_bootstrap_navwalker.php
+++ elements/wp_bootstrap_navwalker.php
@@ -149,7 +149,7 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 				$item_output .= isset( $args->link_before ) ? $args->link_before : '';
 				$item_output .= apply_filters( 'the_title', $item->title, $item->ID );
 				$item_output .= isset( $args->link_after ) ? $args->link_after: '';
-				$item_output .= ( isset( $args->has_children ) && $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
+				$item_output .= ( isset( $args->has_children ) && $args->has_children && 0 === $depth ) ? '<span class="caret"></span></a>' : '</a>';
 				$item_output .= isset( $args->after ) ? $args->after : '';
 				$output      .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 			} // End if().
