0b6d8c213d42fa3bc16550939afc3cbaac661760
Phil T <362017+dtw@users.noreply.github.com>
2024-01-03 18:33:31 +0000

    Add header for ods_status inactive

---
+++ elements/page-title-inactive.php
@@ -0,0 +1,13 @@
+<div class="container-fluid">
+	<div class="row page-header">
+		<div class="container">
+			<div class="row">
+				<!-- HEADING -->
+				<h1>
+						<?php echo 'INACTIVE: ' . get_the_title(); ?>
+				</h1>
+				<!-- DESCRIPTION -->
+				<p>This service has been marked as inactive by NHS Digital and may now be closed</p>
+			</div>
+		</div>
+	<!-- yes, the closing divs are added later-->
---
+++ header-inactive.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+	<?php get_template_part('elements/header-head'); ?>
+
+	<?php get_template_part('elements/header-body-class'); ?>
+
+  <!-- INCLUDE NAVIGATION -->
+
+	<?php get_template_part('elements/nav'); ?>
+
+	<?php get_template_part('elements/page-title-inactive'); ?>
+
+	</div><!-- end of container-fluid -->
