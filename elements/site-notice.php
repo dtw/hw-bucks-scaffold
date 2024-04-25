3ee35450a05e1c2d61888ff9eb24f56fac1c5c7d
Phil T. <362017+dtw@users.noreply.github.com>
2024-04-25 14:16:46 +0100

    Updated the text
    Toned it down a bit...

--- elements/site-notice.php
+++ elements/site-notice.php
@@ -1,7 +1,7 @@
 <div id="hw-scaffold-site-wide-notice" class="row">
   <div class="container">
     <div id="hw-scaffold-site-wide-notice-content">
-      <p>This site is currently experience a major issue and access to linked document files is restricted. We are working to fix this as soon as possible.</p>
+      <p>This site is currently experience an issue and access to some linked document files is restricted. We are working to fix this as soon as possible.</p>
     </div>
   </div>
 </div>
