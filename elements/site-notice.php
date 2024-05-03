9577f175b2e451ba4f4f96a54eb86e96575b4281
Phil T. <362017+dtw@users.noreply.github.com>
2024-05-03 15:15:09 +0100

    Use settings for site-notice

--- elements/site-notice.php
+++ elements/site-notice.php
@@ -1,7 +1,9 @@
 <div id="hw-scaffold-site-wide-notice" class="row">
   <div class="container">
     <div id="hw-scaffold-site-wide-notice-content">
-      <p>This site is currently experience an issue and access to some linked document files is restricted. We are working to fix this as soon as possible.</p>
+      <p>
+        <? echo get_theme_mod('scaffold_site_notice_text') ? get_theme_mod('scaffold_site_notice_text') : 'Set the "Site notice text"'; ?>
+      </p>
     </div>
   </div>
 </div>
--- header.php
+++ header.php
@@ -8,7 +8,9 @@
 <!-- INCLUDE NAVIGATION -->
 
 <?php get_template_part('elements/nav'); ?>
-<?php /* get_template_part('elements/site-notice'); */?>
+<?php if (get_theme_mod('scaffold_site_notice_status')) {
+  get_template_part('elements/site-notice');
+} ?>
 <?php get_template_part('elements/page-title'); ?>
 
 </div><!-- end of container-fluid -->
